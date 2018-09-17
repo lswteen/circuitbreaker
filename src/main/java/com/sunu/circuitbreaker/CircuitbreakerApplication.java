2f5345e398be7d44240e47de6a4dcb62aa2a678b
Unknown <sunu@wemakeprice.com>
2018-09-17 15:08:24 +0900

    master : eureka-client  Add
    add

--- src/main/java/com/sunu/circuitbreaker/CircuitbreakerApplication.java
+++ src/main/java/com/sunu/circuitbreaker/CircuitbreakerApplication.java
@@ -5,12 +5,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 
+@EnableDiscoveryClient
 @EnableCircuitBreaker
 @RestController
 @SpringBootApplication
